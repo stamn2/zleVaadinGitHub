facf80de50c69007927a202138e66839ba75d325
stamn2 <Slin@nilss-macbook.bfh.ch>
2017-04-04 12:37:15 +0200

    chaged

--- zle/src/main/java/com/project1/zle/Main.java
+++ zle/src/main/java/com/project1/zle/Main.java
@@ -4,6 +4,6 @@ public class Main {
 public static void main(String[] args) {
 	System.out.println("hello World!");
 	System.out.println("hello2");
-	System.out.println("YEAHH");
+	System.out.println("YEAHH22s");
 }
 }
