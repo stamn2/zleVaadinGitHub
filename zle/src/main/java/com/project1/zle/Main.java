63feb86d9af9ac63fc3cf8477c24fdb1f852033e
RosalieTruong <rosa.g@windowslive.com>
2017-04-04 16:16:03 +0200

    Test if it works

--- zle/src/main/java/com/project1/zle/Main.java
+++ zle/src/main/java/com/project1/zle/Main.java
@@ -2,6 +2,7 @@ package com.project1.zle;
 
 public class Main {
 public static void main(String[] args) {
-System.out.println("hello from worben");
+	System.out.println("hello from worben");
+	System.out.println("hello, thank you for the mail!");
 }
 }
