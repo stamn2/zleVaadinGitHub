69e120a8cf90afe27f18502b9fa0cbf033598630
stamn2 <Slin@localhost>
2017-04-19 13:54:04 +0200

    main changed

--- zle/src/main/java/com/project1/zle/Main.java
+++ zle/src/main/java/com/project1/zle/Main.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-import com.project1.domain.User;
+import com.project1.domain.Employee;
 
 public class Main {
 	
@@ -19,42 +19,22 @@ public static void main(String[] args) {
 	System.out.println("hello, thank you for the mail!");
 	System.out.println("Test");
 	
-    /*factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-    EntityManager em = factory.createEntityManager();
-    // read the existing entries and write to console
-    Query q = em.createQuery("select t from Todo t");
-    List<Todo> todoList = q.getResultList();
-    for (Todo todo : todoList) {
-        System.out.println(todo);
-    }
-    System.out.println("Size is: " + todoList.size());
-
-    // create new todo
-    em.getTransaction().begin();
-    Todo todo = new Todo();
-    todo.setSummary("This is a test");
-    todo.setDescription("This is a test");
-    em.persist(todo);
-    em.getTransaction().commit();
-
-    em.close();*/
-	
 	factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
     EntityManager em = factory.createEntityManager();
     
-    User user = new User("email@mail.com", "firstname", "lastname", "street", "plz", "city", "tel");
+    Employee user = new Employee("email@mail.com", "firstname", "lastname", "street", "plz", "city", "tel");
     
     em.getTransaction().begin();
     em.persist(user);
     em.getTransaction().commit();
-
+    
     		
-    /*Query q = em.createQuery("select o from User o");
-    List<User> todoList = q.getResultList();
-    for (User todo : todoList) {
+    Query q = em.createQuery("select o from Employee o");
+    List<Employee> todoList = q.getResultList();
+    for (Employee todo : todoList) {
         System.out.println(todo);
     }
-    System.out.println("Size is: " + todoList.size());*/
+    System.out.println("Size is: " + todoList.size());
 
     em.close();
 
