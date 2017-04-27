e2bf412c9fa4c4b48a6f22d3ff0f50d8611d0fb6
stamn2 <Slin@nilss-macbook.bfh.ch>
2017-04-27 13:42:03 +0200

    test done

--- zle/src/test/java/com/project1/test/UserControllerTest.java
+++ zle/src/test/java/com/project1/test/UserControllerTest.java
@@ -2,27 +2,39 @@ package com.project1.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Test;
 
-import com.project1.controller.LoginController;
 import com.project1.controller.UserController;
 import com.project1.entetyManager.ZLEEntityManager;
 
 public class UserControllerTest {
 	private static ZLEEntityManager zem = new ZLEEntityManager();
 	
+
+	@After
+	public void clear(){
+		zem.removeElement(zem.getEmployee("test@mail.com"));
+	}
+	
 	@Test
 	public void addEmployee() {
-		System.out.println(UserController.addEmployee("test@mail.com", "firstname", "lastname", "street", "plz", "city", "tel",true));
-		String pw ="newPassword123";
+		assertTrue(UserController.addEmployee("test@mail.com", "firstname", "lastname", "street", "plz", "city", "tel",true));
 		
-		zem.getEmployee("test@mail.com").setPassword(pw);
+	}
+	
+	@Test
+	public void addSameEmployee() {
+		UserController.addEmployee("test@mail.com", "firstname", "lastname", "street", "plz", "city", "tel",true);
+		assertFalse(UserController.addEmployee("test@mail.com", "firstname", "lastname", "street", "plz", "city", "tel",true));
 		
-		zem.persistObject(zem.getEmployee("test@mail.com"));
+	}
+	
+	@Test
+	public void getActiveEmployees() {
+		UserController.addEmployee("test@mail.com", "firstname", "lastname", "street", "plz", "city", "tel",true);
+		assertEquals("test@mail.com",zem.getActiveEmployees().get(0).getEmail());
 		
-		System.out.println(zem.getEmployee("test@mail.com").checkPassword(pw));
-		System.out.println(LoginController.login("test@mail.com", pw));
-		zem.removeElement(zem.getEmployee("test@mail.com"));
 	}
  
 }
