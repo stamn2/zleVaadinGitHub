5b38ef0809932534d4b855a2c375a7ef0a572ca2
RosalieTruong <rosa.g@windowslive.com>
2017-05-03 17:32:22 +0200

    add activity entity

--- zle/src/main/java/com/project1/domain/Activity.java
+++ zle/src/main/java/com/project1/domain/Activity.java
@@ -1,8 +1,17 @@
 package com.project1.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Activity {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
     private boolean active;
     private Date begin;
     private Date end;
